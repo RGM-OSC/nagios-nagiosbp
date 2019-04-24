5053ca83101ff79dcfc164b08667ad8ae28c47e8
Michael Aubertin <michael.aubertin@gmail.com>
2019-04-24 18:14:13 +0200

    Modify Schema

--- SOURCES/nagiosbp-rgm/schema_nagiosbp.sql
+++ SOURCES/nagiosbp-rgm/schema_nagiosbp.sql
@@ -1,18 +1,49 @@
+-- MySQL dump 10.14  Distrib 5.5.60-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: nagiosbp
+-- ------------------------------------------------------
+-- Server version	5.5.60-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bp`
+--
+
 DROP TABLE IF EXISTS `bp`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bp` (
   `name` varchar(255) NOT NULL,
-  `description` varchar(255) default NULL,
-  `priority` varchar(5) default NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `priority` varchar(5) DEFAULT NULL,
   `type` varchar(3) NOT NULL,
-  `command` varchar(255) default NULL,
-  `url` varchar(255) default NULL,
-  `min_value` varchar(5) default NULL,
-  `is_define` tinyint(1) default '0',
-  PRIMARY KEY  (`name`)
+  `command` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `min_value` varchar(5) DEFAULT NULL,
+  `is_define` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bp`
+--
+
+LOCK TABLES `bp` WRITE;
+/*!40000 ALTER TABLE `bp` DISABLE KEYS */;
+INSERT INTO `bp` VALUES ('rgm_server','RGM_Server_Local','0','ET','','','',1),('RGM','RGM','1','ET','','','',1);
+/*!40000 ALTER TABLE `bp` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `bp_links`
@@ -22,12 +53,22 @@ DROP TABLE IF EXISTS `bp_links`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bp_links` (
-  `id` mediumint(9) NOT NULL auto_increment,
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `bp_name` varchar(255) NOT NULL,
   `bp_link` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bp_links`
+--
+
+LOCK TABLES `bp_links` WRITE;
+/*!40000 ALTER TABLE `bp_links` DISABLE KEYS */;
+INSERT INTO `bp_links` VALUES (1,'RGM','rgm_server');
+/*!40000 ALTER TABLE `bp_links` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `bp_services`
@@ -37,10 +78,31 @@ DROP TABLE IF EXISTS `bp_services`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bp_services` (
-  `id` mediumint(9) NOT NULL auto_increment,
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `bp_name` varchar(255) NOT NULL,
   `host` varchar(255) NOT NULL,
   `service` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bp_services`
+--
+
+LOCK TABLES `bp_services` WRITE;
+/*!40000 ALTER TABLE `bp_services` DISABLE KEYS */;
+INSERT INTO `bp_services` VALUES (1,'rgm_server','localhost.localdomain','Hoststatus'),(2,'rgm_server','localhost.localdomain','memory'),(3,'rgm_server','localhost.localdomain','service_ged'),(4,'rgm_server','localhost.localdomain','processor'),(5,'rgm_server','localhost.localdomain','service_mariadb');
+/*!40000 ALTER TABLE `bp_services` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-24 17:00:57
